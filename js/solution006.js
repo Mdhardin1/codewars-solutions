a4c68c58cda790c63373c8c206c1328e1ac2b623
Davis Hardin <mdhardin1.dev@gmail.com>
2022-05-28 15:15:54 -0500

    comepleted solution006

---
+++ js/solution006.js
@@ -0,0 +1,19 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+let persistence = num => {
+    let multCount = 0;
+    let arr = num.toString().split('');
+    let newNum = num;
+    while (newNum > 9) {
+        newNum = arr.reduce((acc, cur) => acc *= cur, 1)
+        console.log(newNum);
+        arr = newNum.toString().split('');
+    }
+}
+persistence(999);
