f6407d18815a42564a1d5c6dfe965f321a077fdd
Davis Hardin <mdhardin1.dev@gmail.com>
2022-08-30 09:44:21 -0500

    completed solution059

---
+++ js/solution059.js
@@ -0,0 +1,56 @@
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+const alphabet = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+}
+
+function high(x) {
+    let arr= x.split(' ');
+    let max = 0;
+    
+    let result;
+    arr.forEach(x => {
+      let cur = 0;  
+      for (let i = 0; i < x.length; i++) {
+            cur += alphabet[x[i]];
+        }
+        if (cur > max) {
+            max = cur;
+            result = x;
+        }
+    })
+    return result;
+}
